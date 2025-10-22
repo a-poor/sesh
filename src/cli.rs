436724ed726d64ada366504b7b53e8c73ed5ba7e
Austin Poor <45295232+a-poor@users.noreply.github.com>
2025-10-21 17:28:10 -0700

    Sets up some config structs

--- src/cli.rs
+++ src/cli.rs
@@ -1,1 +1,19 @@
 //! App's CLI code.
+
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+pub struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Command {
+    Init,
+    Status,
+    Up,
+    Down,
+    Attach,
+    Restart,
+}
--- src/conf.rs
+++ src/conf.rs
@@ -1,1 +1,14 @@
 //! App configuration structs.
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Config {
+    name: String,
+    window: Vec<WindowConf>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct WindowConf {
+    name: Option<String>,
+    command: Option<Vec<String>>,
+}
